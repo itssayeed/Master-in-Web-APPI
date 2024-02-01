c306b7049fdec8a5fc1afcbdddf0c5406fad6c6c
Sayeed Ahmed <itssayeed.ks@gmail.com>
2024-02-01 10:13:22 +0530

    Removed repository folder as of now not in use

---
+++ Master in Web APPI/Migrations/20240129073908_init.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Master_in_Web_APPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shirts",
+                columns: table => new
+                {
+                    ShirtId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shirts", x => x.ShirtId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Shirts",
+                columns: new[] { "ShirtId", "Brand", "Color", "Gender", "Price", "Size" },
+                values: new object[,]
+                {
+                    { 1, "VN", "Green", "M", 12.199999999999999, 10 },
+                    { 2, "LP", "Maroon", "M", 13.199999999999999, 13 },
+                    { 3, "Coco", "yellow", "F", 13.9, 18 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shirts");
+        }
+    }
+}
